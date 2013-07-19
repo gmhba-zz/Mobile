33265de0b70c8c727c8126010189ecc02abfefad
gmhba <webteam@gmhba.com.au>
2013-07-19 14:56:54 +1000

    initial load
    initial load

---
+++ FirstApp.Solution/platforms/android/gen/R.java.d
@@ -0,0 +1,14 @@
+D:\Development\SVN\Mobile\FirstApp.Solution\platforms\android\gen\firstapp\com\au\R.java \
+ : D:\Development\SVN\Mobile\FirstApp.Solution\platforms\android\res\drawable\icon.png \
+D:\Development\SVN\Mobile\FirstApp.Solution\platforms\android\res\drawable-hdpi\icon.png \
+D:\Development\SVN\Mobile\FirstApp.Solution\platforms\android\res\drawable-hdpi\ic_launcher.png \
+D:\Development\SVN\Mobile\FirstApp.Solution\platforms\android\res\drawable-ldpi\icon.png \
+D:\Development\SVN\Mobile\FirstApp.Solution\platforms\android\res\drawable-ldpi\ic_launcher.png \
+D:\Development\SVN\Mobile\FirstApp.Solution\platforms\android\res\drawable-mdpi\icon.png \
+D:\Development\SVN\Mobile\FirstApp.Solution\platforms\android\res\drawable-mdpi\ic_launcher.png \
+D:\Development\SVN\Mobile\FirstApp.Solution\platforms\android\res\drawable-xhdpi\icon.png \
+D:\Development\SVN\Mobile\FirstApp.Solution\platforms\android\res\drawable-xhdpi\ic_launcher.png \
+D:\Development\SVN\Mobile\FirstApp.Solution\platforms\android\res\layout\main.xml \
+D:\Development\SVN\Mobile\FirstApp.Solution\platforms\android\res\values\strings.xml \
+D:\Development\SVN\Mobile\FirstApp.Solution\platforms\android\res\xml\config.xml \
+D:\Development\SVN\Mobile\FirstApp.Solution\platforms\android\bin\AndroidManifest.xml \
---
+++ FirstApp.Solution/platforms/android/gen/firstapp/com/au/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package firstapp.com.au;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
